bdd1cfa0d63dc82e281470d704631e974b905b38
Kevin DeWall <kevin.h.dewall@gmail.com>
2024-09-07 14:49:01 -0400

    Main fixes and full prototype version

--- dist/config_env.js
+++ dist/config_env.js
@@ -5,6 +5,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const dotenv_1 = __importDefault(require("dotenv"));
 dotenv_1.default.config();
+// Create a constant to be used globally, simple config object can be easily accessable across all other scripts
 const config = {
     GITHUB_TOKEN: process.env.GITHUB_TOKEN,
     LOG_LEVEL: Number(process.env.LOG_LEVEL),
