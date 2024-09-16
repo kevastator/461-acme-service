1820560d7178ed154f9b77685a3a31d500a29ec9
Kevin DeWall <kevin.h.dewall@gmail.com>
2024-09-16 17:06:53 -0400

    NPM Fix Async Parser

--- src/url_parse.ts
+++ src/url_parse.ts
@@ -1,4 +1,4 @@
-export function parseURL(url: string): [string, string]
+export async function parseURL(url: string): Promise<string[]>
 {
     let splitStr: string[] = url.split("/");
     let gitIdx: number = splitStr.indexOf("github.com");
@@ -11,6 +11,40 @@ export function parseURL(url: string): [string, string]
     {
         let npmIdx: number = splitStr.indexOf("www.npmjs.com");
 
-        return ["", ""];
+        if (npmIdx > -1)
+        {
+            // get the package name on npm from the link
+            let packageName: string = splitStr[npmIdx + 2];
+            let repoUrl: string = "";
+
+            // make a fetch call to the npm registry and extract the json response
+            const response = await fetch(`https://registry.npmjs.org/${packageName}`);
+            const data = await response.json()
+
+            // get the hosting url for the repository
+            repoUrl = data?.repository?.url;
+
+            console.log(repoUrl);
+
+            // split the new repo url
+            splitStr = repoUrl.split("/");
+
+            // check if any index of splitStr contains github.com (some cases where there is no perfect github.com)
+            for (let i: number = 0; i < splitStr.length; i++)
+            {
+                if (splitStr[i].includes("github.com"))
+                {
+                    gitIdx = i;
+                }
+            }
+
+            // just like a github link check to see if the index exists
+            if (gitIdx > -1) // NPM link hosts github
+            {
+                return [splitStr[gitIdx + 1], splitStr[gitIdx + 2]]; // return owner and repo name
+            }
+        }
+
+        return ["", ""]; // ERROR INVALID LINK
     }
 }
